679fd96227e05a036284cd9c443219c2ff5318a5
Israel Abiona <adeyanjuabiona69@gmail.com>
2024-01-15 16:31:09 +0100

    Payment fix

--- varsity/main_app/payment_model.py
+++ varsity/main_app/payment_model.py
@@ -5,7 +5,7 @@ class RecipientHoldingAccount(models.Model):
    user = models.OneToOneField(MainUser, on_delete=models.CASCADE, related_name='recipient')
    account_number = models.CharField(max_length=12, unique=True)
    name = models.CharField(max_length=300, null=False)
-   bank_code = models.IntegerField(null=False)
+   bank_code = models.CharField(max_length=7, null=True)
    
    class Meta:
       db_table = 'payment_model_recipientholdingaccount'
